0a7c77c1266aed1903af5027416094278cd5d2eb
feiyan <1285439774@qq.com>
2026-01-31 16:50:26 +0800

    修改release版，无法收到断开连接状态

--- lib/models/connection_status.g.dart
+++ lib/models/connection_status.g.dart
@@ -6,13 +6,23 @@ part of 'connection_status.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ConnectionStatus _$ConnectionStatusFromJson(Map<String, dynamic> json) =>
-    ConnectionStatus(
+ConnectionStatus _$ConnectionStatusFromJson(Map<String, dynamic> json) {
+  if (kReleaseMode) {
+    return ConnectionStatus(
+      json['b'] as String?,
+      json['c'] as String?,
+      json['d'] as String?,
+      connected: json['a'] as bool,
+    );
+  } else {
+    return ConnectionStatus(
       json['message'] as String?,
       json['errorCode'] as String?,
       json['errorDetails'] as String?,
       connected: json['connected'] as bool,
     );
+  }
+}
 
 Map<String, dynamic> _$ConnectionStatusToJson(ConnectionStatus instance) =>
     <String, dynamic>{
